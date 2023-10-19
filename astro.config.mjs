bcf7c6bae4bfe6ac1492c490a337872a68b0da9e
Eilam R <eilam.craft@gmail.com>
2023-10-19 18:26:52 +0100

    github pages fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://circus-real.github.io",
+  base: "/circus-real",
   integrations: [
     tailwind({
       applyBaseStyles: false,
