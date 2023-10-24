6b408f9cd8f15093fff3419e106a2c6594314c69
Eilam R <eilam.craft@gmail.com>
2023-10-24 11:38:18 +0100

    add sitemap, unlighthouse, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://circus-real.github.io/circus-real",
+  site: "https://circus-real.netlify.app",
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
 });
